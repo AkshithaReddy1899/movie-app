ade47937ce7bd7a2ebc756cee2b67380cdf8cf1f
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-02-01 17:52:24 +0530

    Style main page

--- src/modules/render.js
+++ src/modules/render.js
@@ -6,13 +6,18 @@ const Render = (movie) => {
   container.innerHTML = `<img src="${movie.show.image.original}" alt=""/>
   <div class="item-header-container">
   <h2 class="name">${movie.show.name}</h2>
-  <h3 class="like">5 likes</h3>
+  <p class="like">5 likes</p>
   </div>`;
   const button = document.createElement('button');
   button.className = 'openModal';
   button.type = 'button';
   button.textContent = 'Comments';
+
+  const reservBtn = document.createElement('button');
+  reservBtn.textContent = 'Reservations';
+
   container.appendChild(button);
+  container.appendChild(reservBtn);
   itemContainer.appendChild(container);
 };
 
