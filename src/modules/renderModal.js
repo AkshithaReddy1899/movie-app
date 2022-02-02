f46589bc7b08f21dd23bfc98e63b56288a3cf13d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-02-02 15:31:11 +0500

    add involments

--- src/modules/renderModal.js
+++ src/modules/renderModal.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
+export const getCounts = (arr) => arr.length;
+
+// eslint-disable-next-line no-unused-vars
+const incrementCount = (title, commentsInfo) => {
+  const value = getCounts(commentsInfo) + 1;
+  title.textContent = `Comments (${value})`;
+};
+
 const renderModal = async (movie) => {
   const modalContainer = document.getElementById('modal-container');
 
@@ -11,10 +20,50 @@ const renderModal = async (movie) => {
       <p>Language: ${movie.show.language}</p>
       <p>Type: ${movie.show.type}</p>
       </div>
+      <h4 class="comments-title my-2">Comments</h4>
+          <ul class="comments-list list-group list-unstyled"></ul>
+          <h5 class="add-comment my-4">Add a comment</h5>
+          <form class="d-flex flex-column align-items-start" action="#">
+            <input
+                class="form-control"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Your name"
+            /><br />
+            <textarea
+                class="form-control"
+                type="text"
+                id="insight"
+                name="insight"
+                placeholder="Your insights"
+                rows="4" 
+                cols="50"
+            ></textarea><br />
+            <input class="btn btn-dark" type="submit" id="submit-btn" value="Comment" />
       `;
   document.getElementById('close').addEventListener('click', () => {
     document.getElementById('modal-container').classList.remove('active');
     document.getElementById('overlay').classList.remove('active');
+  });
+
+  const name = document.querySelector('#name');
+  const insight = document.querySelector('#insight');
+  const form = document.querySelector('form');
+  const title = document.querySelector('.comments-title');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    postCommentToApi(showId, name.value, insight.value);
+    addItemToList(ul, [
+      {
+        creation_date: '2021-08-04',
+        username: name.value,
+        comment: insight.value,
+      },
+    ]);
+    name.value = '';
+    insight.value = '';
+    incrementCount(title, commentsInfo);
   });
 };
 
